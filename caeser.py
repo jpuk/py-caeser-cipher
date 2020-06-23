95b105d62dfa5b0fd02a61c863188a5484ad3388
John <john.jporter@gmail.com>
2020-06-23 13:11:17 +0100

    first commit - caeser cipher

---
+++ caeser.py
@@ -0,0 +1,39 @@
+low_ascii = 65
+high_ascii = 90
+magic_number = 7
+alpha = [chr(i) for i in range(low_ascii, high_ascii+1)]
+
+def encode(inString):
+    outString = []
+    for char in inString:
+        try:
+            index = alpha.index(char)
+            valid_char = alpha[index]
+        except ValueError:
+            outString.append(char)
+            continue
+
+        if (index + low_ascii) > (high_ascii - magic_number):
+            valid_char = alpha[index - (len(alpha) - magic_number)]
+            outString.append(valid_char)
+        else:
+            outString.append(chr(index + low_ascii + magic_number))
+    return outString
+
+
+def decode(inString):
+    outString = []
+    for char in inString:
+        try:
+            index = alpha.index(char)
+        except ValueError:
+            outString.append(char)
+            continue
+
+        if index < magic_number:
+            decoded_char = (alpha.index(char) + len(alpha) - magic_number) + low_ascii
+        else:
+            decoded_char = alpha.index(char) + low_ascii - magic_number
+        outString.append(chr(decoded_char))
+    return outString
+
